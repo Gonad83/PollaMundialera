5720c09d381e135a661bc2e79165f3f0e34b4612
Gonzalo Araos <garaosd@gmail.com>
2026-05-28 14:22:35 -0400

    feat: agregar banner interactivo para pronosticar la final de la UCL

--- mundial2026-frontend/vite.config.js
+++ mundial2026-frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+    },
+  },
   plugins: [
     react(),
     VitePWA({
